09bcb4e291407c626cfd262ac9320a07df427f33
pankaj mahor <pmahor@immply.local>
2017-09-14 15:28:45 +0530

    Dependencies Added

--- src/main/java/FirstPack/First_Maven_proj/First_Test_Class.java
+++ src/main/java/FirstPack/First_Maven_proj/First_Test_Class.java
@@ -1,9 +1,43 @@
 package FirstPack.First_Maven_proj;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+@Test
 public class First_Test_Class {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver","D:\\Pankaj BACKUP\\Selenium\\Workspace\\First_Selenium_Project\\libraries\\chromedriver.exe");
+		WebDriver driver =new ChromeDriver(); 
+		// FirefoxDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("http://toolsqa.com/");
+		
+		//driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
+		
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS );
+		
+		//Thread.sleep(20000);
+		
+		WebElement next = driver.findElement(By.xpath(".//*[@id='slide-16-layer-5']/a"));
+		next.click();
+		
+		driver.navigate().back();
+		Thread.sleep(20000);
+		
+		driver.navigate().forward();
+		
+		driver.navigate().refresh();
+		
+		driver.close();
+
 
 	}
 
